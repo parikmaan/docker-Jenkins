3d55450be71272de1ec1ece3b5e40f284e03027b
Parik <25786988+parikmaan@users.noreply.github.com>
2017-11-24 21:17:12 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-docker run -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -d --name docker-jenkins docker-jenkins
+docker run -d --name docker-jenkins -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock docker-jenkins
